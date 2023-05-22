966c8dcd0f70891abc93695514f7972761e97750
jjcinaz <jjc@leapscape.com>
2023-05-22 15:49:51 -0700

    Added function to get IPv6 Settings

---
+++ ipv6settings.go
@@ -0,0 +1,41 @@
+package gotik
+
+import (
+	"errors"
+)
+
+type IPv6Settings struct {
+	Forward                    bool
+	AcceptRedirects            string
+	AcceptRouterAdvertisements string
+	MaxNeighborEntries         int
+}
+
+func parsev6Settings(props map[string]string) IPv6Settings {
+	entry := IPv6Settings{
+		Forward:                    parseBool(props["forward"]),
+		AcceptRedirects:            props["accept-redirects"],
+		AcceptRouterAdvertisements: props["accept-router-advertisements"],
+		MaxNeighborEntries:         parseInt(props["max-neighbor-entries"]),
+	}
+	return entry
+}
+
+// GetIPv6Settings returns the current IPv6 settings
+func (c *Client) GetIPv6Settings(baseIntf string) (settings IPv6Settings, err error) {
+	var detail *Reply
+	if detail, err = c.Run("/ipv6/settings/print"); err != nil {
+		return
+	}
+
+	// we only want one result
+	switch len(detail.Re) {
+	case 0:
+		err = ErrNotFound
+	case 1:
+		settings = parsev6Settings(detail.Re[0].Map)
+	default:
+		err = errors.New("unexpected return")
+	}
+	return
+}
